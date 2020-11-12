acfb53f08d92297f5c294f1105da307a715f1daf
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-11-12 15:12:50 -0800

    added basic copy to Getting Started component

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -12,7 +12,32 @@ export const GettingStarted = props => {
         Getting Started
       </div>
       <Divider/>
-      ** helpful info here **
+      <div>
+        <div>
+          Welcome to Relakqs!
+        </div>
+        <div>
+          Overview
+        </div>
+        <div>
+          Relakqs is a chatting application with separate channels, each with their own topic!
+        </div>
+        <div>
+          Creating a channel
+        </div>
+        <div>
+          To create a new channel, click the plus icon next to the 'Channels' tab on the sidebar and enter the name of your new channel.
+          This channel name must be different from previous channels, or you when you hit enter you will be prompted to join the original.
+          When you create a channel you become the admin of that channel and you have the ability to set the topic!
+          If you are the admin of a channel a 'Edit Topic' button will appear and you wil be able to change the topic!
+        </div>
+        <div>
+          Changing your avatar or bio
+        </div>
+        <div>
+          To change your avatar or bio, simply click on the avatar in the upper left hand corner and edit your info on the popover!
+        </div>
+      </div>
     </div>
   )
 };
